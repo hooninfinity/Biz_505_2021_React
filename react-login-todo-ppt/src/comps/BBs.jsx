1f3b1b3858bc73d29d8e9c64586868a38b6d5fb4
hooninfinity <hooninfinity@naver.com>
2021-10-09 20:07:15 +0900

    ppt

---
+++ react-login-todo-ppt/src/comps/BBs.jsx
@@ -0,0 +1,22 @@
+import { useUserContext } from "../context/UserContextProvider";
+import TodoMain from "../comps/TodoMain";
+import TodoInput from "../comps/TodoInput";
+import TodoList from "../comps/TodoList";
+
+function BBs() {
+  const { user } = useUserContext();
+
+  return (
+    <div>
+      <div>
+        <h1>자유게시판</h1>
+        <h3>USERID : {user.userid}</h3>
+      </div>
+      <TodoMain header="My TodoList" form={<TodoInput />}>
+        <TodoList />
+      </TodoMain>
+    </div>
+  );
+}
+
+export default BBs;
